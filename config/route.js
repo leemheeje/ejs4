1bb279e42e6ec36b10ac00010d09e294a9abbb06
leemheeje <dlagmleo89@gmail.com>
2025-03-17 14:13:52 +0900

    Update route.js

--- config/route.js
+++ config/route.js
@@ -6,11 +6,9 @@ module.exports = async (dirPath, filename) => {
   const jsonFilePath = path.join(__dirname, '../.routes', dirPath, `${filename.replace('.ejs', '')}.js`)
   const objectRegex = /defineExport\s*\(\s*(\{[\s\S]*?\})\s*\)\.bind\(\)/
   const data = await fs.readFile(ejsFilePath, 'utf-8')
-  const match = objectRegex.exec(data)
-  if (match) {
-    const jsonContent = `const i18n = require('i18n')\nconst $t = i18n.__\n\nmodule.exports = ${match[1]}`
-    const directPath = path.dirname(jsonFilePath)
-    await fs.mkdir(directPath, {recursive: true})
-    await fs.writeFile(jsonFilePath, jsonContent, 'utf-8')
-  }
+  const match = objectRegex.exec(data) || ['', '{}']
+  const jsonContent = `const i18n = require('i18n')\nconst $t = i18n.__\n\nmodule.exports = ${match[1]}`
+  const directPath = path.dirname(jsonFilePath)
+  await fs.mkdir(directPath, {recursive: true})
+  await fs.writeFile(jsonFilePath, jsonContent, 'utf-8')
 }
